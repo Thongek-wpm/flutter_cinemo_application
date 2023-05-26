8d8f31495c45ea80139175f0674409661486aa16
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-26 18:03:11 +0700

    Initial commit

---
+++ lib/screens/favoriteUi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavoireUi extends StatefulWidget {
+  const FavoireUi({super.key});
+
+  @override
+  State<FavoireUi> createState() => _FavoireUiState();
+}
+
+class _FavoireUiState extends State<FavoireUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/viewUi.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewUi extends StatelessWidget {
+  const ViewUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
