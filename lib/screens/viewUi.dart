46a4ddeffe378866e9e53475a273b48bf7dd8429
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-29 14:04:22 +0700

    Update viewUi.dart

--- lib/screens/viewUi.dart
+++ lib/screens/viewUi.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cinemo_application/src/feed.dart';
 
 class ViewUi extends StatelessWidget {
-  const ViewUi(movi, {super.key});
+  final Movie movies;
+  const ViewUi(this.movies, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        title: const Text(
+          'Cinemo',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Row(children: [
+          ClipRect(
+            child: Image.network(''),
+          )
+        ]),
+      ),
+    );
   }
 }
